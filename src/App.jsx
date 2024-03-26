079259d66513b94626468c07d1b2c9fbc493b713
rotem25 <rotemsh37@gmail.com>
2024-03-26 17:30:47 +0200

    app.jsx doc

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
+
+//Imports necessary hooks and components from React, React Router, and local modules
 import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import { routes } from "./routes";
 import { Sidebar } from "./components/Sidebar";
 import { useEffect } from "react";
 import { Logo } from "./components/Logo";
 
+
 function App() {
+//Hooks for programmatically navigating between routes and accessing the current location.
   const navigate = useNavigate();
   const location = useLocation();
-
+//hecks for an existing "email" key in local storage to determine the user's logged-in status.
   const email = localStorage.getItem("email");
 
   const isLoggedIn = !!email;
 
   useEffect(() => {
+    //Contains logic for redirecting users based on their authentication
+    //state and current location. It ensures that unauthenticated users 
+    //cannot access certain pages and that logged-in users are redirected from 
+    //the login/register pages to the home page.
     const currentPageIsRegister = location.pathname.startsWith("/register");
     const currentPageIsLogin = location.pathname.startsWith("/login");
 
