35499a961c32261239c3b7c2cd350e6bb81771ee
rotem25 <rotemsh37@gmail.com>
2024-03-29 19:27:57 +0300

    documentation

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,21 +1,23 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
-
+//Create a Context object.
 const ThemeContext = createContext();
 
 export const useTheme = () => useContext(ThemeContext);
 
 export const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
-
+ // Effect hook to perform side effects.
+ // In this case, it updates localStorage and the document's data-theme attribute whenever the theme changes.
   useEffect(() => {
     localStorage.setItem('theme', theme);
     document.documentElement.setAttribute('data-theme', theme);
-  }, [theme]);
-
+  }, [theme]);//This effect depends on the theme state; it reruns whenever the theme changes.
+//Function to toggle the theme between 'light' and 'dark'.
   const toggleTheme = () => {
     setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
   };
-
+   //The provider component from Context is used to pass down
+   //the theme state and the toggleTheme function to any child components that might need them.
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
       {children}
