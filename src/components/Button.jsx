954538a3706e0c5f5f7f1815cd722fb956feba42
yaelkanter <yaelkan21@gmail.com>
2024-03-31 05:09:33 +0300

    coments components

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,17 @@
 import React from "react";
-
+// Button component that allows customization and handles click events
 export const Button = ({ children, onClick, className, type }) => {
   return (
+    // Render a button element with dynamic attributes and styles.
     <button
+     // Set the button type, defaulting to "button" if no type is provided
       type={type || "button"}
       onClick={onClick}
+      //The default styling includes a black background, white border and text, padding, and rounded corners.
       className={`bg-black border-2 transition duration-400 roundedbg-black border-white text-white py-2 hover:bg-pink-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500
       px-4 rounded-md text-base ${className || ""}`}
     >
+     
       {children}
     </button>
   );
