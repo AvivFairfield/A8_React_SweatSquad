723f6b730e27bfb32e1093d3b3be5e0f378ac7b3
Aviv Cohen <avivcohen22@icloud.com>
2024-04-02 17:21:20 +0300

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      backgroundImage: {
-        page: "url(https://i.ibb.co/Rc3rZQF/most-colors-modified-image.png)",
-      },
-    },
-  },
-  plugins: [],
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	darkMode: "class",
+	theme: {
+		extend: {
+			backgroundImage: {
+				page: "url(https://i.ibb.co/Rc3rZQF/most-colors-modified-image.png)",
+			},
+		},
+	},
+	plugins: [],
 };
