622ae15070aba16edc31fd05f7bc7f27766c6710
Aviv Cohen <avivcohen22@icloud.com>
2024-04-03 15:57:05 +0300

    kek

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
+import ThemeContext from "../context/ThemeContext";
 
 // This component renders the company's logo.
 export const Logo = () => {
+	const { theme, toggleTheme } = useContext(ThemeContext);
+
 	return (
 		// The logo image is rendered within an <img> tag.
 		// It uses Tailwind CSS classes for styling:
@@ -11,7 +14,11 @@ export const Logo = () => {
 		<img
 			className="logo-img block w-28"
 			alt="logo"
-			src="https://i.ibb.co/4jnQ609/white-logo.png" // The source URL of the logo image.
+			src={
+				theme == "dark"
+					? `https://i.ibb.co/4jnQ609/white-logo.png`
+					: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRozUF_IOKpEGe22IryUa6uFLbmuAR9QIVX6lSY_Rr6uQ&s"
+			} // The source URL of the logo image.
 		/>
 	);
 };
