22eafc6bb823738554fd9da6a6802d50799a70e7
rotem25 <rotemsh37@gmail.com>
2024-04-19 13:19:02 +0300

    adding notes

--- src/api.js
+++ src/api.js
@@ -1,18 +1,26 @@
+//Performs a GET request to a specified API endpoint.
+
 export async function getRequest(url) {
 	try {
+		//Fetches data from the server. Await pauses until the fetch completes.
 		const response = await fetch(
 			"https://sweatsquadapi.onrender.com" + url
 		);
 
+		//Parses the response body as JSON. Await pauses until the parsing completes.
 		return await response.json();
 	} catch (error) {
+		//Logs the error to the console
 		console.error("Error:", error);
+		//Rethrows the error to be handled by the calling function
 		throw error;
 	}
 }
+//Performs a POST request to a specified API endpoint with a JSON body.
 
 export async function postRequest(url, body) {
 	try {
+		
 		const response = await fetch(
 			"https://sweatsquadapi.onrender.com" + url,
 			{
